e497774448bcdcbb83650ccdbaaf4b8d9bf0f793
Sarah Aldous <sealdous@hotmail.com>
2019-03-19 17:15:01 -0600

    added axios stuff

--- client/src/components/unauthenticated/Login.js
+++ client/src/components/unauthenticated/Login.js
@@ -5,9 +5,11 @@ const Login = () => {
         <form className="login">
             <span>Username: </span>
             <input/>
-            <span>Password: </span>
-            <input/>
+           
             <button>Login</button>
+            <div>
+                
+            </div>
         </form>
     )
 }

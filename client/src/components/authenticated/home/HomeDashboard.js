53829a34905331954a01f3543a6f783532c8857f
Meghan Bomberger <meghanbomberger@gmail.com>
2019-03-20 09:56:23 -0600

    added start of custom greeting to home dashboard

--- client/src/components/authenticated/home/HomeDashboard.js
+++ client/src/components/authenticated/home/HomeDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 //COMPONENT
 import Post from "./Post.js"
@@ -10,22 +10,52 @@ import Unicorn from '../../avatars/007-unicorn.png'
 import Narwhal from '../../avatars/011-narwhal.png'
 
 
-const HomeDashboard = () => {
-    return (
-        <section className="home-dashboard">
-            <div>
-                <Post avatar={Dragon} name="Dragon" title="has reached Journeyman LvL in tax-preparation"/>
-                <Post avatar={Mermaid} name="Mermaid" title="has posted a new quest attempt"/>
-                <Post avatar={Unicorn} name="Unicorn" title="has posted a new quest attempt"/>
-                <Post avatar={Narwhal} name="Narwhal" title="has reached Novice LvL in dish washing"/>
-            </div>
-            <div className="status-updates">
-                <p>You have <b>5</b> new quest validations</p>
-                <p>Your <b>folding laundry</b> quest attempt has been validated. You are now a <b>Master Dishwasher</b>.</p>
-                <p>You have <b>3</b> unshared quest attempts</p>
-            </div>
-        </section>
-    )
+class HomeDashboard extends Component {
+    constructor(){
+        super()
+        this.state = {
+            greeting: "",
+        }
+    }
+
+    componentDidMount(){
+        const currentDate = new Date()
+        const currentHour = currentDate.getHours()
+        let greeting = ""
+        if (currentHour < 12) {
+            greeting = "Good morning"
+        } else if (currentHour > 12 && currentHour < 16) {
+            greeting = "Good afternoon"
+        } else if (currentHour > 16) {
+            greeting = "Good evening"
+        } else {
+            greeting = "Hello"
+        }
+        this.setState({
+            greeting: greeting,
+        })
+    }
+
+    render(){
+        //Update to autopopulate player name
+        return (
+            <section className="home-dashboard">
+                <h5>{this.state.greeting}, Player Name</h5>
+                <div>
+                    <Post avatar={Dragon} name="Dragon" title="has reached Journeyman LvL in tax-preparation"/>
+                    <Post avatar={Mermaid} name="Mermaid" title="has posted a new quest attempt"/>
+                    <Post avatar={Unicorn} name="Unicorn" title="has posted a new quest attempt"/>
+                    <Post avatar={Narwhal} name="Narwhal" title="has reached Novice LvL in dish washing"/>
+                </div>
+                <div className="status-updates">
+                    <p>You have <b>5</b> new quest validations</p>
+                    <p>Your <b>folding laundry</b> quest attempt has been validated. You are now a <b>Master Dishwasher</b>.</p>
+                    <p>You have <b>3</b> unshared quest attempts</p>
+                </div>
+            </section>
+        )
+    }
+    
 }
 
 export default HomeDashboard
---
+++ client/src/components/authenticated/questlog/InProgressQuests.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const InProgressQuests = () => {
+    return (
+        <section className="in-progress-quest-log">
+            In Progress Quests
+        </section>
+    )
+}
+
+export default InProgressQuests

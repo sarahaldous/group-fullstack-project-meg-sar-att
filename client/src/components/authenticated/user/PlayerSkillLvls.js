20238920a7b578fcb6b5786a9c8bc03780072d58
Meghan Bomberger <meghanbomberger@gmail.com>
2019-03-19 23:45:53 -0600

    user dashboard layout complete

---
+++ client/src/components/authenticated/user/PlayerSkillLvls.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PlayerSkillLvls = () => {
+    return (
+        <div className="skill-bar-container">
+            <p>Category</p>
+            <div className="skill-bar">
+                <div className="skill-progress-meter"></div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default PlayerSkillLvls

e687a0908ed79630b272975f2e93eaba381047e2
Sarah Aldous <sealdous@hotmail.com>
2019-03-26 10:52:49 -0600

    welcome message works

--- client/src/TempNavBar.js
+++ client/src/TempNavBar.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import { withPlayer } from './context/PlayerProvider';
 
-const TempNavBar = () => {
+class TempNavBar extends Component {
+    render(){
+    
+    var welcomeMessage = `Welcome, ${this.props.name}`
     return (
         <nav className="temp-nav-bar">
             <Link to="/site/home">Home</Link>
             <Link to="/">Logout</Link>
-            <Link to="/admin">Admin</Link>    
+            <Link to="/admin">Admin</Link>  
+            <h3>{welcomeMessage}</h3>  
         </nav>
     )
-};
+}
+}
 
-export default TempNavBar
+export default withPlayer(TempNavBar)
